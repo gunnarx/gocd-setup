a13d28e8a3b2ddd9b5df3ee1fda7169255b78c39
Gunnar Andersson <gand@acm.org>
2015-11-18 16:33:01 +0100

    Rename newuser script and remove interactive printouts

--- newuser.sh
+++ newuser.sh
@@ -2,8 +2,12 @@
 # Encode a password using SHA1, then base64 as used by Go password file
 # (unless you're using LDAP instead)
 
-echo 'Enter pass, end with return'
+[ -z "$1" ] && {
+   echo "Usage: $0 <username>"
+   echo "Then enter password on standard-in and hit return"
+   exit 1
+}
+
 read x
 p=$(/bin/echo -n "$x" | sha1sum | awk '{print $1'} | xxd -r -p | base64)
-echo "$p"
-
+echo "$1:$p"
