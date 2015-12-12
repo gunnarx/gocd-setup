7004997194b6a80dd8e004771497c44331091f31
Gunnar Andersson <gand@acm.org>
2015-12-12 16:00:46 +0100

    Fixing buildnotify install

--- install_buildnotify.sh
+++ install_buildnotify.sh
@@ -10,7 +10,7 @@ echo "--------------------------------------------------------"
 
 . ./go_server || { echo  "Can't find go_server definition" ; exit 1 ; }
 
-CCURL=$GO_SERVER_URL:$GO_SERVER_PORT/go/cctray.xml
+CCURL="http://buildmonitor:<ask_someone_for_the_password>@$GO_SERVER:$GO_SERVER_PORT/go/cctray.xml"
 
 # Modern fedora uses dnf, earlier use yum, ubuntu/debian uses apt-get
 installer=UNKNOWN
@@ -32,7 +32,9 @@ else
    echo "*** Assuming Fedora OS - if using arch, suse or other, please edit script!"
    echo Installing using Python PIP easy_install
    sudo $installer -q install python-pip pyqt4
+   sudo pip install --upgrade pip               # might as well...
    sudo pip install pytz
+   sudo pip install python-dateutil
    sudo easy_install BuildNotify
 fi
 
@@ -41,8 +43,13 @@ echo "Done (check for errors above)"
 echo
 echo "Setup: If all is OK, you can run buildnotifyapplet.py"
 echo "Then find it in the tray, right click -> Preferences"
+echo
 echo "Then add monitoring for this url: "
 echo "$CCURL"
+echo
+echo "WARNING!  Buildmonitor does not support SSL, therefore"
+echo "you should NOT use your personal account & password - ask for"
+echo "the password to the limited buildmonitor account instead!"
 echo "--------------------------------------------------------"
 
 
