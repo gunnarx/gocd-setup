44a5067c663173151caa5f4c4bffa9d6039bcde8
Gunnar Andersson <gand@acm.org>
2015-12-12 14:32:19 +0100

    Wrapper script all-in-one

---
+++ go_agent_install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Yet another wrapper around go-agent install.  This one is all-in-one,
+# and can be downloaded and piped to a shell directly:
+# curl https://raw.githubusercontent.com/gunnarx/gocd-setup/master/go_agent_install.sh | bash
+
+[ -x "$(which yum)" ] && installer=yum && init=true
+[ -x "$(which dnf)" ] && installer=dnf && init=true
+[ -x "$(which apt-get)" ] && installer=apt-get && init="sudo apt-get update"
+
+$init                                               && \
+sudo $installer -y install git                      && \
+git clone http://github.com/gunnarx/gocd-setup      && \
+cd gocd-setup                                       && \
+./go-agent-setup.sh
