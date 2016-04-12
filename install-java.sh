7b891f1207f08478f1b027e3be8dd3ef6f6506c8
Gunnar Andersson <gand@acm.org>
2016-04-12 10:10:57 +0200

    Remove superfluous rpm/dpkg in install-java

--- install-java.sh
+++ install-java.sh
@@ -6,12 +6,10 @@ type=$(./rpm-or-deb.sh)
 case $type in
    rpm)
       sudo yum install -y java-1.8.0-openjdk
-      sudo rpm -iv $path || fail "RPM install failed"
       ;;
    deb)
       sudo apt-get update
       sudo apt-get install -y openjdk-8-jre
-      sudo dpkg -i $path || fail "DEB install failed"
       ;;
    *)
       fail "Could not determin rpm/deb type?"
