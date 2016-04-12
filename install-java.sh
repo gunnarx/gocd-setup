1eb4461b933f67317b99ad85248712a0be54e96e
Gunnar Andersson <gand@acm.org>
2016-04-12 10:57:48 +0200

    Need unzip also

--- install-java.sh
+++ install-java.sh
@@ -5,11 +5,11 @@
 type=$(./rpm-or-deb.sh)
 case $type in
    rpm)
-      sudo yum install -y java-1.7.0-openjdk
+      sudo yum install -y java-1.7.0-openjdk unzip
       ;;
    deb)
       sudo apt-get update
-      sudo apt-get install -y openjdk-7-jre
+      sudo apt-get install -y openjdk-7-jre unzip
       ;;
    *)
       fail "Could not determin rpm/deb type?"
