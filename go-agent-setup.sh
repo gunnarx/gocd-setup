13cb064fffaf5bb3e68f9006668688e4fce5918c
Gunnar Andersson <gand@acm.org>
2016-04-18 18:37:56 +0200

    Refactor and make go-agent script similar to go-server

--- go-agent-setup.sh
+++ go-agent-setup.sh
@@ -2,54 +2,104 @@
 # (C) 2015 Gunnar Andersson
 # License: CC-BY-4.0 (https://creativecommons.org/licenses/by/4.0/)
 
+# ---------------------------------------------------------------------------
 # SETTINGS
+# ---------------------------------------------------------------------------
 version=16.3.0-3183
 
+# Normalize directory - make sure we start in "this" directory
 D=$(dirname "$0")
 cd "$D"
 MYDIR="$PWD"
 
 fail() { echo "Something went wrong - check script" 1>&2 ; echo $@ 1>&2 ; exit 1 ; }
 
-# GET AGENT
-file=$(./download.sh agent $version)
 
-# Install Java (see script for version)
+# ---------------------------------------------------------------------------
+# Function: Configure JAVA_HOME variable
+# ---------------------------------------------------------------------------
+# This is slightly overcomplicated and can probably be rewritten.  Also this
+# might be done correctly by the post-install scripts in the deb/rpm but if it
+# fails here is another attempt. Anyway, the point is to figure out JAVA_HOME
+# and add it to the config file.
+
+add_java_to_conf() {
+   sudo cp /etc/default/go-agent /tmp/newconf.$$ || fail "copying conf"
+   sudo chmod 666 /tmp/newconf.$$ || fail "conf?"
+
+   javadir=$(ls /usr/lib/jvm | egrep "java-.*-openjdk-.*$" | head -1)
+   java_home=/usr/lib/jvm/$javadir/jre
+   [ -d "$java_home" ] || fail "Could not figure out JAVA_HOME directory - please check the script"
+   [ -x "$java_home/bin/java" ] || fail "Could not find java executable in JAVA_HOME ($java_home) - please check the script"
+
+   export JAVA_HOME="$java_home"
+   cat <<EEE >>/tmp/newconf.$$
+   export JAVA_HOME="$java_home"
+EEE
+
+   # OK, put it back and just in case, fix up permissions and stuff
+   sudo mv /tmp/newconf.$$ /etc/default/go-agent
+   sudo chown root:root /etc/default/go-agent
+   sudo chmod 644 /etc/default/go-agent
+}
+
+# MAIN SCRIPT STARTING -- agent
+
+# ---------------------------------------------------------------------------
+# Download and install agent (helper script)
+# ---------------------------------------------------------------------------
+path=$(./download.sh agent $version)
+
+# ---------------------------------------------------------------------------
+# Install Java, git and stuff.  N.B.: Java version is coded into helper script.
+# ---------------------------------------------------------------------------
 ./install-java.sh
+./install-prerequisites.sh
 
-[ -f "$file" ] || fail "No go-agent installation archive found"
+[ -f "$path" ] || fail "No go-agent installation archive found"
 
+# ---------------------------------------------------------------------------
+# Install the rpm/deb previously downloaded
+# ---------------------------------------------------------------------------
+type=$(./rpm-or-deb.sh)
+case $type in
+   rpm)
+      sudo rpm -iv $path || fail "RPM install failed"
+      ;;
+   deb)
+      sudo dpkg -i $path || fail "DEB install failed"
+      ;;
+   *)
+      fail "Unsupported package type - fix script"
+      ;;
+esac
+
+# ---------------------------------------------------------------------------
+# Setting up directories
+# ---------------------------------------------------------------------------
+
+# Most (all?) these dirs are likely created by deb/rpm installation
+# but creating them just in case.
 echo "Fixing install/log directories to be accessible for go user"
-sudo mkdir -p /var/{log,lib}/go-agent
-sudo chown -R go:go /var/{log,lib}/go-agent
+sudo mkdir -m 755 -p /var/{log,lib,run}/go-agent $GO_HOME_DIR || fail "Can't create /var/... directories"
+sudo chown -R go:go /var/{log,lib,run}/go-agent $GO_HOME_DIR || fail "Can't chown a directory"
 
 [ -f go-agent.conf ] || fail "Can't find go-agent.conf in this directory - giving up"
 
 echo Copying default conf to /etc/default/go-agent
 sudo cp go-agent.conf /etc/default/go-agent
 
-# This tries to figure out the actual path to openJDK, which includes
-# the minor-version number and such things that might change in the future.
-java_home=/usr/lib/jvm/$(ls /usr/lib/jvm/ | egrep 'java-.*-openjdk-.*$' | head -1)/jre
+add_java_to_conf
 
-[ -d "$java_home" ] || fail "Could not figure out JAVA_HOME directory - please check the script"
-[ -x "$java_home/bin/java" ] || fail "Could not find java executable in JAVA_HOME ($java_home) - please check the script"
-
-echo "Adding JAVA_HOME to config file"
-cp /etc/default/go-agent /tmp/newconf.$$
-sudo chmod 666 /tmp/newconf.$$
-cat <<EEE >>/tmp/newconf.$$
-export JAVA_HOME="$java_home"
-EEE
-
-echo GO_SERVER is set to:
+echo Go-agent is installed - NOTE: It will contact the go server
+echo at the defined address: GO_SERVER is set to:
 fgrep GO_SERVER= /tmp/newconf.$$
 
-# OK, put it back and just in case, fix up permissions and stuff
-sudo mv /tmp/newconf.$$ /etc/default/go-agent
-sudo chown root:root /etc/default/go-agent
-sudo chmod 644 /etc/default/go-agent
-
-echo "Done.  Try running agent with: "
-echo 'sudo su go -c "/etc/init.d/go-agent start"'
+echo
+echo Current Status:
+service go-agent status
+echo "Try starting with"
+echo "sudo service go-agent start"
+echo "otherwise with:"
+echo 'sudo -u go /etc/init.d/go-agent start'
 
