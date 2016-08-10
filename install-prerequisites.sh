de07da2ad8b62bcd7c1094f0c59bf3a9de01d535
gunnarx <gunnarx@users.noreply.github.com>
2016-08-10 17:53:38 +0200

    Ensure curl is installed

--- install-prerequisites.sh
+++ install-prerequisites.sh
@@ -3,11 +3,11 @@
 type=$(./rpm-or-deb.sh)
 case $type in
    rpm)
-      sudo yum install -y unzip git
+      sudo yum install -y unzip git curl
       ;;
    deb)
       sudo apt-get update
-      sudo apt-get install -y unzip git
+      sudo apt-get install -y unzip git curl
       ;;
    *)
       fail "Could not determine rpm/deb type?"
