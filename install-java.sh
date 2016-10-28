924d2ac8803b9246558fb44567280743de0064d0
Gunnar Andersson <gand@acm.org>
2016-10-28 14:08:40 +0200

    Try Java 8

--- install-java.sh
+++ install-java.sh
@@ -3,11 +3,11 @@
 type=$(./rpm-or-deb.sh)
 case $type in
    rpm)
-      sudo yum install -y java-1.7.0-openjdk unzip
+      sudo yum install -y java-1.8.0-openjdk unzip
       ;;
    deb)
       sudo apt-get update
-      sudo apt-get install -y openjdk-7-jre unzip
+      sudo apt-get install -y openjdk-8-jre unzip
       ;;
    *)
       fail "Could not determine rpm/deb type?"
