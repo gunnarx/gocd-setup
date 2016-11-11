fc58382f1d1b24e59a491057c3091d76c731594d
Gunnar Andersson <gand@acm.org>
2016-11-11 11:56:26 +0100

    Quickfix libsystemd-daemon-dev / libsystemd-dev name change

--- install_common_build_dependencies.sh
+++ install_common_build_dependencies.sh
@@ -91,4 +91,13 @@ case $installer in
       ;;
 esac
 
+# Bugfix deb packages that have changed names
+osrelease=/etc/os-release
+fgrep -q Ubuntu $osrelease && fgrep -q 14.04 $osrelease && sudo apt-get install libsystemd-daemon-dev
+fgrep -q Ubuntu $osrelease && fgrep -q 16.04 $osrelease && sudo apt-get install libsystemd-dev
+# FIXME - not sure which version needs what here...
+fgrep -q Debian $osrelease && sudo apt-get install libsystemd-daemon-dev || {
+   echo "Failed?  Not sure on systemd package name, but no problem - trying another:"
+   sudo apt-get install libsystemd-dev
+}
 
