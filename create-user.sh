97cfb68dd14fd430a0b68d8f17cd006b0773159c
Gunnar Andersson <gand@acm.org>
2017-01-26 14:41:43 +0100

    Minor spell fix + two scripts that might be useful

---
+++ create-user.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# ---------------------------------------------------------------------------
+# Function: Create user
+# ---------------------------------------------------------------------------
+
+# Once I found that go-server installer created the go user but go-agent 
+# installer did not?
+
+# Not sure if this is needed any longer and it depends on if it is done already.
+# Leaving it here just in case...
+
+[ $(id -u) -eq 0 ] || { echo "Please make sure you run as root to create users" ; exit 1 ; }
+
+create_user() {
+  [ -n "$1" ] || { echo "Usage: create_user <username>" ; return 1 ; }
+
+  id "$1" >/dev/null 2>&1
+  if [ $? != 0 ] ; then
+     groupadd "$1" --gid 1500
+     useradd "$1" -g "$1" --uid 1500 -d "$GO_HOME_DIR"
+  else
+     echo "User $1 exists -- skipping"
+  fi
+}
+
---
+++ docker/setup-host.sh
@@ -0,0 +1,4 @@
+# Dir shared between gousers container and go-server
+# to store the user/password file.
+mkdir /gousers
+
