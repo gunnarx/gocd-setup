9ad8efcf5a7047e8a56f1701bd869154545c9c15
Gunnar Andersson <gand@acm.org>
2017-02-14 20:15:51 +0100

    clean/fix go-config-cronjob

--- go-config-cronjob.sh
+++ go-config-cronjob.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-# Backup (push) server config to git repo
-su go -c "cd $CRUISE_CONFIG_DIR && git push backup master"
+CRUISE_CONFIG_DIR=/var/lib/go-server/db/config.git/
 
+# Backup (push) server config to git repo
+cd $CRUISE_CONFIG_DIR
+git push backup master
