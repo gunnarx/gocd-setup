101f6d84ba4d4c9cffcaf65893863c53d958ea14
Taha Mohammad <taha.mohammad.i03@gmail.com>
2017-03-02 16:47:56 +0100

    Update go-agent-config-cronjob.sh

--- go-agent-config-cronjob.sh
+++ go-agent-config-cronjob.sh
@@ -1,2 +1,2 @@
 #! /bin/sh 
-cd /gocd-setup && /usr/bin/git pull && apt-get install $(cat common_build_dependencies)
+cd ~/gocd-setup && /usr/bin/git pull && apt-get install $(cat common_build_dependencies)
