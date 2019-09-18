5523cc75fb60c2d9482dfd4521fd121cd49ce42f
Gunnar Andersson <gandersson@genivi.org>
2019-09-18 10:58:49 +0200

    Add script to install lavacli

--- install-lava-testing.sh
+++ install-lava-testing.sh
@@ -7,12 +7,19 @@
 LAVACLI_URL=https://files.pythonhosted.org/packages/b1/e4/958e3c2b027e0ff6efbc2f135d893baa848755e486d284bcaf88ac574417/lavacli-0.9.6.tar.gz
 LAVACLI_DIR=$(basename "$LAVACLI_URL" | sed 's/.tar.gz//')
 
-wget -c "$LAVACLI_URL"
-tar xf "$(basename "$LAVACLI_URL")"
-cd "$LAVACLI_DIR"
-
 # This step requires python 3 of course, and the setuptools package... and
 # possibly some other python deps
+LAVACLI_FILE="$(basename "$LAVACLI_URL")"
+LAVACLI_DIR="$(echo "$LAVACLI_FILE" | sed 's/.tar.gz//')"
+
+curl -O -J -L "$LAVACLI_URL"
+tar xf "$LAVACLI_FILE"
+
+sudo apt-get install -y python3-setuptools
+sudo pip3 install --upgrade pip
+sudo pip install pyzmq PyYaml jinja2 setuptools
+
+cd "$LAVACLI_DIR"
 sudo python3 setup.py install
 
 echo Testing lavacli:
