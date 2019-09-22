283c9eed654b9f15ed7da9685ac035cf9257694f
Gunnar Andersson <gandersson@genivi.org>
2019-09-22 20:02:16 +0200

    lavacli: Update to 0.9.8 + use curl

--- install-lava-testing.sh
+++ install-lava-testing.sh
@@ -4,16 +4,16 @@
 # test-jobs into a Lava test infrastructure.
 
 # Update this version as needed...
-LAVACLI_URL=https://files.pythonhosted.org/packages/b1/e4/958e3c2b027e0ff6efbc2f135d893baa848755e486d284bcaf88ac574417/lavacli-0.9.6.tar.gz
+LAVACLI_URL=https://files.pythonhosted.org/packages/82/b3/2de414b62995b1e2084cdd0f3d978c3a4df8dc46e436fa23f9a820c5990b/lavacli-0.9.8.tar.gz
 LAVACLI_FILE="$(basename "$LAVACLI_URL")"
 LAVACLI_DIR="$(echo "$LAVACLI_FILE" | sed 's/.tar.gz//')"
 
 curl -O -J -L "$LAVACLI_URL"
 tar xf "$LAVACLI_FILE"
 
-sudo apt-get install -y python3-setuptools
+sudo apt-get install -y python3-setuptools python3-pip
 sudo pip3 install --upgrade pip
-sudo pip install pyzmq PyYaml jinja2 setuptools
+sudo pip3 install pyzmq PyYaml jinja2 setuptools
 
 cd "$LAVACLI_DIR"
 sudo python3 setup.py install
